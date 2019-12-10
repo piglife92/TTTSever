d241d6b1d0d2e285ccd5eb195f385cf9e9562d66
Youngjae Park <54255680+piglife92@users.noreply.github.com>
2019-12-10 15:46:33 +0900

    비밀번호 암호화 진행

--- tictactoe-server/routes/users.js
+++ tictactoe-server/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -20,8 +21,6 @@ router.post('/signup', function(req, res, next) {
     return;
   }
 
-
-
   var validate = userValidation(username, password);
   if (validate == false){
     res.json({message:'400 Bad Request'});
@@ -36,17 +35,28 @@ router.post('/signup', function(req, res, next) {
       res.json({message:'400 Bad Request'});
       return;
     } else{
-      usersCollection.insertOne({'username': username,
-       'password': password, 'name': name}, function(err, result){
-          if(err) throw(err);
-          if(result.ops.length > 0)
-            res.json(result.ops[0]);
-          else
-            res.json({message:'503 Server Error'});
+      //var cryptoPassword = crypto.createHash('sha512').update(password).digest('base64');
+
+      crypto.randomBytes(64, function(err, buf) {
+        
+        const saltStr = buf.toString('base64');
+        crypto.pbkdf2(password, saltStr , 100, 64, 'sha512', function(err, key) {
+          const cryptoPassword = key.toString('base64');
+
+          usersCollection.insertOne({'username': username,
+           'password': cryptoPassword, 'name': name, 'salt': saltStr}, function(err, result){
+            if(err) throw(err);
+            if(result.ops.length > 0)
+              res.json(result.ops[0]);
+            else
+              res.json({message:'503 Server Error'});
+          });
+        });
       });
+
+      
     }
   });
-
   
 });
 
@@ -68,6 +78,11 @@ var userValidation = function(username, password) {
 
  /* 로그인 */
 router.post('/signin', function(req, res, next){
+  var username = req.body.username;
+  var password = req.body.password;
+  var name = req.body.name;
+  
+  var db = req.app.get('database');
 
 });
  
